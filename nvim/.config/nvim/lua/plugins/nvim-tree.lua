aba4131f3b73b47388be183d4677a640c9f3336d
mara <mara@the-witch.ru>
2022-01-12 20:55:41 +0300

    nvim-tree show root folder

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -42,7 +42,7 @@ require('nvim-tree').setup({
   view = {
     width = 30,
     height = 30,
-    hide_root_folder = true,
+    hide_root_folder = false,
     side = 'right',
     auto_resize = true,
     mappings = {
